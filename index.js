8bdc690d6b921ed5fe00f3a041888ab8b94ad364
Michal <khakaimic4@gmail.com>
2023-03-16 00:13:47 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+// Write your code 
+   
+    const num1 = 31;
+    const num2 = 2;
+    const multiply= num1*num2;
+    
+
+// 2
+    const random = Math.ceil(Math.random()* 10)+1 ;
+
+ //3
+     const num3 = 16;
+     const num4 = 6;
+     const mod = 4
+     num3 % num4 === 4;
+
+
+//4
+const max = Math.max(5, 10, 15, 20);
+
